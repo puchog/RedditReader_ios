d181e4c16dd8ce778b430a34b3c10ca088d138bd
Juan Giannuzzo <juan.giannuzzo@bairesdev.com>
2017-02-02 10:05:04 -0300

    Added initial files

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# A Simple Reddit Reader iOS project
---
+++ RedditReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedditReader
+//
+//  Created by Juan Giannuzzo on 2/2/17.
+//  Copyright © 2017 Deviget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
